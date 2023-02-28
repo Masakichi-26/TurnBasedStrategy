23f257eb3274b020dcddd2b876de0a57c9cea053
Masakichi-26 <54612863+Masakichi-26@users.noreply.github.com>
2023-02-28 21:16:38 +0900

    Lesson 54 - Ragdoll

---
+++ Assets/Scripts/UnitRagdollSpawner.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class UnitRagdollSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform ragdollPrefab;
+    [SerializeField] private Transform originalRootBone;
+    [SerializeField] private HealthSystem healthSystem;
+
+    private void Awake()
+    {
+        healthSystem.OnDead += HealthSystem_OnDead;
+    }
+
+    private void HealthSystem_OnDead(object sender, EventArgs e)
+    {
+        Transform ragdollTransform = Instantiate(ragdollPrefab, transform.position, transform.rotation);
+        UnitRagdoll unitRagdoll = ragdollTransform.GetComponent<UnitRagdoll>();
+        unitRagdoll.Setup(originalRootBone);
+    }
+}
